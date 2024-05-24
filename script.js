2df78914baa18c749549a9aa27fe46cb06614976
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2024-05-24 19:56:57 -0300

    first commit - portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+// Mudando titulo quando o usuário troca de aba.
+let docTitle = document.title;
+
+window.addEventListener("blur", () => {
+  document.title = "🍓 Ei, volta aqui! 🥦";
+});
+
+window.addEventListener("focus", () => {
+  document.title = docTitle;
+});
